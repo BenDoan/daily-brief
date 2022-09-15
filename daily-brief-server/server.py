d06a22e8c01327c32383e44d22ca0a92fb21b9d0
Ben Doan <ben@bendoan.me>
2022-09-14 23:00:05 -0500

    add basic auth

--- daily-brief-server/server.py
+++ daily-brief-server/server.py
@@ -1,11 +1,13 @@
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
 app.mount("/img", StaticFiles(directory="../data/img"), name="static")
 app.mount("/_app", StaticFiles(directory="../data/build/_app"), name="static")
 app.add_middleware(
@@ -21,10 +23,10 @@ def load_reddit_data():
         return json.load(f)
 
 @app.get("/api/data")
-def read_root():
+def read_root(credentials: HTTPBasicCredentials = Depends(security)):
     return load_reddit_data()
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
+async def read_root(credentials: HTTPBasicCredentials = Depends(security)):
     with open("../data/build/index.html") as f:
         return "".join(f.readlines())
